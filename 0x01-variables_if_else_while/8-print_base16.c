d220cf47a47e6afbef975ba45712c9493e37c053
chihebbenslama <chihebbenslama@gmail.com>
2023-06-16 18:29:34 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints
+ *
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		putchar ('0' + n);
+		n++
+	}
+	while ((n > 9) && (n < 16))
+	{
+		putchar ('W' + n);
+		n++
+	}
+	putchar('\n');
+	return (0);
+}
