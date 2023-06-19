ed1d26c664b594edf091698c62bba702a693d209
chihebbenslama <chihebbenslama@gmail.com>
2023-06-19 01:07:34 +0000

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *  *main - Prints all possible combinations of two two-digit numbers,
- *   *        ranging from 0-99, separated by a comma followed by a space.
- *    *
- *     * Return: Always 0.
- *      */
+ * main - Prints all possible combinations of two two-digit numbers,
+ * ranging from 0-99, separated by a comma followed by a space.
+ *
+ * Return: Always 0 (success)
+*/
 int main(void)
 {
 	int i, j;
