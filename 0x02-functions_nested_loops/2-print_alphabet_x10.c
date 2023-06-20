572e068c2e3fbf6e6ab951d84efcdefebc64a7d2
chihebbenslama <chihebbenslama@gmail.com>
2023-06-20 15:33:13 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - Prints 10 x alphabet
+ *
+ *
+ * Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		print_alphabet();
+		n++;
+	}
+	_putchar('\n');
+	return (0);
+}
