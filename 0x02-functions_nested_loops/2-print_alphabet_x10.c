4541ed902aca99c7ea39d2cf4ba2f88defce6b9d
chihebbenslama <chihebbenslama@gmail.com>
2023-06-20 16:01:50 +0000

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,20 @@
  * print_alphabet_x10 - Prints 10 x alphabet
  *
  *
- * Return: Always 0 (Success)
+ *
 */
 void print_alphabet_x10(void)
 {
 	int i;
+	char c;
 
 	for (i = 0; i < 10; i++)
 	{
-		print_alphabet();
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+			_putchar('n');
+		}
 	}
 	return;
 }
