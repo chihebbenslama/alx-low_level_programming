1f29562602ac025f62ec32e97e9e75cab9adb897
chihebbenslama <chihebbenslama@gmail.com>
2023-06-20 23:12:42 +0000

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,9 +11,9 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	for (i = 0; i >= 23; i++)
+	for (i = 0; i <= 23; i++)
 	{
-		for (j = 0; j >= 59; i++)
+		for (j = 0; j <= 59; i++)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
