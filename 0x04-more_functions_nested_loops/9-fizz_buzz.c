2fc800c8cd84308c0dfc3c1b1b7f1b3ac7de0620
chihebbenslama <chihebbenslama@gmail.com>
2023-06-22 17:13:52 +0000

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,20 +7,22 @@
  * else print buzz if i is multiples of five
  * else print fizzbuzz if i is multiple of five and three
  * otherwise prints i
+ *
+ * Return: Always 0 (success).
 */
 int main(void)
 {
 	for (i = 1; i <= 99; i++)
 	{
 		if (i % 3 == 0)
-		printf("Fizz");
+		printf("Fizz ");
 		else if (i % 5 == 0)
-			printf("Buzz");
+			printf("Buzz ");
 		else if ((i % 3 == 0) && (i % 5 == 0))
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else
-			printf("%d", i);
-		putchar(' ');
+			printf("%d ", i);
 	}
 	printf("Buzz");
+	return (0);
 }
