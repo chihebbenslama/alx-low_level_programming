b296a6d539fa682e280cdaba10d3f769bf37a2f5
chihebbenslama <chihebbenslama@gmail.com>
2023-06-26 21:29:52 +0000

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,11 +14,12 @@ void puts2(char *str)
 	int i, len;
 
 	len = strlen(str);
-	for (i = 0; i < len - 1; i++)
+	for (i = 0; i < len; i++)
 	{
 		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
 	}
+	_putchar('\n');
 }
