566c2a0caddc8ac5b8ae13fc502df211165c617d
chihebbenslama <chihebbenslama@gmail.com>
2023-06-26 21:55:16 +0000

    Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
 #include "main.h"
 #include <string.h>
-#include <2-strlen.c>
 
 /**
- * main - Prints a random number and states whether
- * it is positive, negative, or zero.
+ * puts_half - prints half of a string, followed by a new line.
+ * @str: string to be printed
  *
  *
 */
 void puts_half(char *str)
 {
-	int len = _strlen(str);
+	int i, len, half;
 
-	printf("%d\n", len);
+	len = strlen(str);
+	half = strlen / 2;
+	for (i = half; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+
+
 }
