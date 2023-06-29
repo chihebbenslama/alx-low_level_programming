f9fabb2fffd54d557786a5a9dfad78c7a18b20d0
chihebbenslama <chihebbenslama@gmail.com>
2023-06-29 12:57:08 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - copies a string.
+ * @dest: string
+ * @char: string
+ * @n: n bytes from src
+ *
+ * Return: the string copied
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
