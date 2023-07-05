7c9aa0e5296a23831aed4d936d98655a1231e481
chihebbenslama <chihebbenslama@gmail.com>
2023-07-05 15:00:30 +0000

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main_h"
+
+/**
+ * mult - return natural suqare root of n
+ * @a: multpiles
+ * @b: the integer n
+ *
+ * Return: return natural suqare root of b
+*/
+int mult(int a, int b)
+{
+	if (a * a == b)
+		return (b);
+	else if (a * a > b)
+		return (-1);
+	return (mult(a - 1, b));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: integer
+ *
+ * Return: return -1 if n does not have a natural square root else
+ * return it's natural suquare root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (mult(1, n));
+}
